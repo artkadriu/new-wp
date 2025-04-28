e717651ae287064fb88232b0f378a6d82ed60910
artkadriu <artkadriu80@gmail.com>
2025-04-28 17:47:54 +0200

    dasda
    asdsda

--- archive.php
+++ archive.php
@@ -1,11 +1,13 @@
 <?php get_header(); ?>
 
+
 <img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" />
 
         <div id="content" class="site-content">
             <div id="primary" class="content-area">
                 <main id="main" class="site-main">
-                    <h1>Blog</h1>
+                <?php the_archive_title('<h1> class="archive-title">','</h1');?>
+                <?php the_archive_description('<div class="archive-description">''</div');?>
                     <div class="container">
                         <div class="blog-items">
                             <?php 
